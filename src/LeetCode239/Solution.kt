080152e19ff55f51e7869657be30d665e30a5813
Harera <61611892+hassan0shaban@users.noreply.github.com>
2021-10-21 07:14:39 +0200

    LeetCode 173

---
+++ src/LeetCode239/Solution.kt
@@ -0,0 +1,62 @@
+package LeetCode239
+
+import java.util.*
+import kotlin.Comparator
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
+
+fun main() {
+
+    println(
+        maxSlidingWindow(
+            arrayOf(
+                1,3,1,2,0,5
+            ).toIntArray(),
+            3
+        ).toMutableList()
+    )
+}
+
+fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+    val pq = PriorityQueue<Int>(
+        object : Comparator<Int> {
+            override fun compare(o1: Int, o2: Int): Int {
+                return o2 - o1
+            }
+        }
+    )
+
+    val hm = HashMap<Int, Int>()
+    val ans = ArrayList<Int>()
+
+    var idx = 0
+    while (idx < k) {
+        if (hm.containsKey(nums[idx]))
+            hm.put(nums[idx], hm[nums[idx]]!! + 1)
+        else
+            hm.put(nums[idx], 1)
+        pq.add(nums[idx])
+        idx++
+    }
+    ans.add(pq.poll())
+
+    while (idx < nums.size) {
+        pq.clear()
+
+        if (hm[nums[idx - k]]!! > 1)
+            hm.put(nums[idx - k], hm[nums[idx - k]]!! - 1)
+        else
+            hm.remove(nums[idx - k])
+
+        if (hm.containsKey(nums[idx]))
+            hm.put(nums[idx], hm[nums[idx]]!! + 1)
+        else
+            hm.put(nums[idx], 1)
+
+        pq.addAll(hm.keys)
+        ans.add(pq.poll())
+        idx++
+    }
+    return ans.toIntArray()
+}
