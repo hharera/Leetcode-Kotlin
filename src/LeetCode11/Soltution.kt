494d376e4b2a2046022e30d289341998ec675a56
Harera <61611892+hassan0shaban@users.noreply.github.com>
2021-10-28 07:14:55 +0200

    LeetCode 11

--- src/LeetCode11/Soltution.kt
+++ src/LeetCode11/Soltution.kt
@@ -1,46 +1,29 @@
 package LeetCode11
 
-import kotlin.math.abs
-import kotlin.math.max
-import kotlin.math.min
-
-
-data class Bar(
-    val idx: Int,
-    val height: Int,
-) : Comparable<Bar> {
-    override fun compareTo(other: Bar): Int {
-        if (other.height == height)
-            return other.idx - idx
-        return height - other.height
-    }
-}
-
 fun maxArea(height: IntArray): Int {
-    return search(0, height.lastIndex, height.asList())
-}
+    var ans = Int.MIN_VALUE
+    var start = 0
+    var end = height.lastIndex
 
-fun search(start: Int, end: Int, heights: List<Int>): Int {
-    if (start == end) {
-        return 0
-    }
-    var max = Int.MIN_VALUE
-    var height = min(heights[start], heights[end])
-    max = max(height * (end - start), max)
+    while (start < end) {
+        val size = (end - start) * if (height[start] > height[end]) height[end] else height[start]
+        if (ans < size)
+            ans = size
 
-    if (heights[start] < heights[end]) {
-        max = max(max, search(start + 1, end, heights))
-    } else {
-        max = max(max, search(start, end - 1, heights))
+        if (height[start] > height[end])
+            end--
+        else
+            start++
     }
-    return max
+
+    return ans
 }
 
 fun main() {
     println(
         maxArea(
             arrayOf(
-                1, 2, 1
+                1, 1
             ).toIntArray()
         )
     )
