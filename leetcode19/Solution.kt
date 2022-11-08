3fbe046e6161a560f63939c64b38e1da6e5ba7e6
hassan-harera <hassan.shaban.harera@gmail.com>
2022-11-08 06:48:19 +0200

    leetcode 19

--- leetcode19/Solution.kt
+++ leetcode19/Solution.kt
@@ -1,58 +1,64 @@
 package com.harera.leetcode.leetcode19
 
-fun main() {
-    var result = (
-        removeNthFromEnd(
-            ListNode(1).apply {
-                next = ListNode(2).apply {
-                    next = ListNode(3).apply {
-                        next = ListNode(4).apply {
-                            next = ListNode(5)
-                        }
-                    }
-                }
-            }, 2
-        )
-    )
-    while (result != null) {
-        println(result.`val`)
-        result = result.next
-    }
-}
-
-fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
-    if (head == null)
-        return head
-
-    var cur = head
-    var size = 0
-    while (cur != null) {
-        size++
-        cur = cur.next
-    }
-
-    if (size == 1) {
-        return null
-    }
-
-    if (size == n) {
-        return head.next
-    }
-
-    cur = head.next
-    var _previous = head
-    var count = 0
-    while (count < size - n - 1) {
-        cur = cur!!.next
-        _previous = _previous!!.next
-        count++
-    }
-    _previous!!.next = cur!!.next
-
-    return head
-}
-
 
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
+}
+
+class Solution {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        if (head == null)
+            return null
+
+        var tempHead = head
+        var size = 0
+        while (tempHead != null) {
+            size++
+            tempHead = tempHead.next
+        }
+
+        if (size == 1) {
+            return null
+        }
+
+        if (size == n) {
+            return head.next
+        }
+
+        tempHead = head
+        var idx = 1
+        while (true) {
+            if (idx == size - n) {
+                tempHead?.next = tempHead?.next?.next
+                break
+            }
+            tempHead = tempHead!!.next
+            idx++
+        }
+        return head
+    }
+}
+
+
+fun main() {
+    arrayOf(
+        ListNode(1).apply {
+            next = ListNode(2).apply {
+                next = ListNode(3).apply {
+                    next = ListNode(4).apply {
+                        next = ListNode(5)
+                    }
+                }
+            }
+        },
+        ListNode(1).apply {
+            next = ListNode(2)
+        },
+        ListNode(1),
+        null,
+    ).forEach {
+        println(
+            Solution().removeNthFromEnd(it, 1)?.`val`
+        )
+    }
 }
