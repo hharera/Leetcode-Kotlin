c99d509bfee354de858219d71b189862032b6f03
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2023-03-02 08:10:26 +0200

    leetcode 124

--- leetcode124/Solution.kt
+++ leetcode124/Solution.kt
@@ -1,45 +1,61 @@
-package LeetCode124
+package com.harera.leetcode.leetcode124
 
-import LeetCode173.TreeNode
 import kotlin.math.max
 
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class Solution {
+
+    private var globalMax = Int.MIN_VALUE
+
+    fun maxPathSum(root: TreeNode?): Int {
+        if (root == null)
+            return 0
+
+        sum(root)
+
+        return globalMax
+    }
+
+    private fun sum(root: TreeNode?): Int {
+        if (root == null)
+            return 0
+
+        val left = max(0, sum(root.left))
+        val right = max(0, sum(root.right))
+        val cur = root.`val`
+
+        val localSum = cur + left + right
+        globalMax = max(localSum, globalMax)
+
+        val maxLocalPath = cur + max(left, right)
+        return maxLocalPath
+    }
+}
+
 fun main() {
-    println(
-        maxPathSum(
-            TreeNode(-10).apply {
-                left = TreeNode(9).apply {
-
-                }
-
-                right = TreeNode(20).apply {
-                    left = TreeNode(7).apply {
-
-                    }
-
-                    right = TreeNode(15).apply {
-
-                    }
-                }
-            }
-        ).toString())
-}
-
-var max = Int.MIN_VALUE
-fun maxPathSum(root: TreeNode?): Int {
-    return solve(root, 0)
-}
-
-fun solve(root: TreeNode?, sum: Int): Int {
-    if (root == null)
-        return sum
-
-    val left = max(
-        solve(root.left, sum),
-        solve(root.left, sum + root.`val`)
-    )
-
-    solve(root.right, sum)
-    solve(root.right, sum + root.`val` + left)
-
-    return sum
+    val solution = Solution()
+    // write 5 test cases with results
+    println(solution.maxPathSum(TreeNode(1).apply {
+        left = TreeNode(2)
+        right = TreeNode(3)
+    }))
+    println(solution.maxPathSum(TreeNode(-10).apply {
+        left = TreeNode(9)
+        right = TreeNode(20).apply {
+            left = TreeNode(15)
+            right = TreeNode(7)
+        }
+    }))
+    println(solution.maxPathSum(TreeNode(2).apply {
+        left = TreeNode(-1)
+    }))
+    println(solution.maxPathSum(TreeNode(-3)))
+    println(solution.maxPathSum(TreeNode(1).apply {
+        left = TreeNode(-2)
+        right = TreeNode(3)
+    }))
 }
