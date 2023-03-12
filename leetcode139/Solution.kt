11ca0718a66fb92fc208e589d5df779b15a18c4a
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2023-03-12 11:27:54 +0200

    leetcode 139

---
+++ leetcode139/Solution.kt
@@ -0,0 +1,76 @@
+package com.harera.leetcode.leetcode139
+
+class Solution {
+
+    private val dp = Array<Boolean?>(305) { null }
+
+    fun wordBreak(raw: String, wordDict: List<String>): Boolean {
+        return dfs(raw, 0, wordDict)
+    }
+
+    fun dfs(raw: String, idx: Int, wordDict: List<String>): Boolean {
+        if (idx > raw.lastIndex)
+            return true
+
+        if (dp[idx] != null)
+            return dp[idx]!!
+
+        var result = false
+        for (word in wordDict) {
+            if (raw.startsWith(word, idx)) {
+                result = result or dfs(raw, idx + word.length, wordDict)
+            }
+        }
+
+        return result.also {
+            dp[idx] = it
+        }
+    }
+}
+
+
+fun main() {
+    // 8 test cases
+    println(
+        Solution().wordBreak(
+            "leetcode",
+            listOf(
+                "leet", "code"
+            )
+        )
+    )
+    println(
+        Solution().wordBreak(
+            "applepenapple",
+            listOf(
+                "apple", "pen"
+            )
+        )
+    )
+    println(
+        Solution().wordBreak(
+            "catsandog",
+            listOf(
+                "cats", "dog", "sand", "and", "cat"
+            )
+        )
+    )
+    println(
+        Solution().wordBreak(
+            "aaaaaaa",
+            listOf(
+                "aaaa", "aaa"
+            )
+        )
+    )
+    println(
+        Solution().wordBreak(
+            "cars",
+            listOf(
+                "car", "ca", "rs"
+            )
+        )
+    )
+
+    println(Solution().wordBreak("leetcode", listOf("leet", "code")))
+}
