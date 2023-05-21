bc6f25e74520eac488f65a629d9f8f0dd7bf9cac
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2023-05-21 14:12:27 +0300

    leetcode 432

--- leetcode432/Solution.kt
+++ leetcode432/Solution.kt
@@ -1,5 +1,66 @@
-package LeetCode432
+package com.harera.leetcode.leetcode432
 
+import java.util.*
+import java.util.AbstractMap.SimpleEntry
+
+class AllOne() {
+
+    private val maxQ = PriorityQueue<Map.Entry<String, Int>> { a, b ->
+        b.value - a.value
+    }
+
+    private val minQ = PriorityQueue<Map.Entry<String, Int>> { a, b ->
+        a.value - b.value
+    }
+    private val keys = HashMap<String, Int>()
+    fun inc(key: String) {
+        if (keys[key] != null) {
+            keys[key] = keys[key]!! + 1
+        } else {
+            keys.put(key, 1)
+        }
+        minQ.add(SimpleEntry(key, keys[key]!!))
+        maxQ.add(SimpleEntry(key, keys[key]!!))
+    }
+
+    fun dec(key: String) {
+        if (keys[key]!! == 1) {
+            keys.remove(key)
+        } else {
+            keys[key] = keys[key]!! - 1
+            minQ.add(SimpleEntry(key, keys[key]!!))
+            maxQ.add(SimpleEntry(key, keys[key]!!))
+        }
+    }
+
+    fun getMaxKey(): String {
+        if (keys.isEmpty())
+            return ""
+
+        var peek = maxQ.peek()
+        while (maxQ.isNotEmpty() && peek != null && keys[peek.key] != peek.value) {
+            maxQ.poll()
+            peek = maxQ.peek()
+        }
+        if (maxQ.peek() == null)
+            return ""
+        return maxQ.peek().key
+    }
+
+    fun getMinKey(): String {
+        if (keys.isEmpty())
+            return ""
+
+        var peek = minQ.peek()
+        while (minQ.isNotEmpty() && peek != null && keys[peek.key] != peek.value) {
+            minQ.poll()
+            peek = minQ.peek()
+        }
+        if (minQ.peek() == null)
+            return ""
+        return minQ.peek().key
+    }
+}
 
 fun main() {
     AllOne().apply {
@@ -10,67 +71,5 @@ fun main() {
         inc("leet")
         println(getMaxKey())
         println(getMinKey())
-    }
-}
-
-
-data class Key(
-    val key: String,
-    val count: Int
-)
-
-class AllOne() {
-    private val keys = HashMap<String, Int>()
-
-    fun inc(key: String) {
-        if (keys[key] != null) {
-            keys[key] = keys[key]!! + 1
-        } else {
-            keys.put(key, 1)
-        }
-    }
-
-    fun dec(key: String) {
-        if (keys[key]!! == 1) {
-            keys.remove(key)
-        } else {
-            keys[key] = keys[key]!! - 1
-        }
-    }
-
-    fun getMaxKey(): String {
-        if (keys.isEmpty())
-            return ""
-
-        var max = Int.MIN_VALUE
-        var key = ""
-
-        keys.forEach {
-            max = if (max < it.value) {
-                key = it.key
-                it.value
-            }
-            else
-                max
-        }
-        return key
-    }
-
-    fun getMinKey(): String {
-        if (keys.isEmpty())
-            return ""
-
-        var min = Int.MAX_VALUE
-        var key = ""
-
-        keys.forEach {
-            min = if (min > it.value) {
-                key = it.key
-                it.value
-            }
-            else
-                min
-        }
-        return key
     }
 }
