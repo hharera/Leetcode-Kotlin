88c002a92518723f7665274fd5a0b236a0e5dab5
hharera <hassan.shaban.harera@gmail.com>
2024-01-02 18:26:10 +0200

    leetcode 166

--- leetcode166/Solution.kt
+++ leetcode166/Solution.kt
@@ -5,28 +5,33 @@ class Solution {
     fun fractionToDecimal(numerator: Int, denominator: Int): String {
         val divResult = (numerator.toFloat() / denominator.toFloat())
         val fractionStr = divResult.toString().split(".")[1]
-        val possibleFractions = mutableListOf<String>()
 
         if (fractionStr.toInt() == 0)
             return divResult.toInt().toString()
 
-        var str = ""
-        fractionStr.forEach {
-            str += it
-            possibleFractions.add(str)
-        }
+        if (fractionStr.length == 1)
+            return divResult.toString()
 
-        possibleFractions.reversed().forEach {
-            var str = fractionStr
-            val index = str.indexOf(it)
-            str = str.removeRange(index, index + it.length)
-            if (str.contains(it))
-                return divResult.toString().split(".")[0].plus(".").plus("($it)")
+        for (length in 1 until fractionStr.length) {
+            if (check(fractionStr, length)) {
+                return divResult.toString().split(".")[0].plus(".").plus("(${fractionStr.substring(0, length)})")
+            }
         }
         return divResult.toString()
+    }
+
+    private fun check(fullFraction: String, length: Int): Boolean {
+        var remaining = fullFraction
+        val str = fullFraction.substring(0, length)
+        while (remaining.isNotBlank()) {
+            if (!remaining.contains(str))
+                return false
+            remaining = remaining.removeRange(0, length)
+        }
+        return true
     }
 }
 
 fun main() {
-    println(Solution().fractionToDecimal(4, 333))
+    println(Solution().fractionToDecimal(1, 6))
 }
