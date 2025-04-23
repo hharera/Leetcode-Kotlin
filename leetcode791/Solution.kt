a348654a4d07e44682bd891fe78f112c6186a1ca
Hassan Harera <Hassa.Shaban.Harera@gmail.com>
2025-04-23 21:02:48 +0200

    update leetcode 291

--- leetcode791/Solution.kt
+++ leetcode791/Solution.kt
@@ -3,6 +3,22 @@ package com.harera.leetcode.leetcode791
 
 class Solution {
     fun customSortString(order: String, s: String): String {
+        val chars = HashMap<Char, Int>()
+        var remaining = ""
+        s.forEach {
+            chars[it] = chars.getOrDefault(it, 0) + 1
+            if (order.contains(it).not())
+                remaining += it
+        }
+
+        var result = ""
+        order.forEach {
+            result += "$it".repeat(chars.getOrDefault(it, 0))
+        }
+        return result + remaining
+    }
+
+    fun customSortString_v1(order: String, s: String): String {
         val toChar = HashMap<Int, Char>()
         val toInt = HashMap<Char, Int>()
 
