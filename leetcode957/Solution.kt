eedb7ebaac6de3dbbc777984936a9c021ce8c573
Hassan Harera <Hassa.Shaban.Harera@gmail.com>
2025-04-27 23:03:53 +0300

    update leetcode 957

--- leetcode957/Solution.kt
+++ leetcode957/Solution.kt
@@ -1,44 +1,43 @@
 package com.harera.leetcode.leetcode957
 
 class Solution {
-    fun prisonAfterNDays(_cells: IntArray, n: Int): IntArray {
-        val map = HashMap<String, Int>()
-        val days = HashMap<Int, IntArray>()
+    fun prisonAfterNDays(_cells: IntArray, _n: Int): IntArray {
+        val map = HashMap<Int, String>()
 
-        var cells = _cells.copyOf()
-        if (n > 1000000) {
-            var day = 1
+        var n = 0
+        var cells = _cells
+        while (n < 15) {
             cells = next(cells)
-            while (map.contains(cells.contentToString()).not()) {
-                days[day] = cells
-                map[cells.contentToString()] = day
-                day++
-                cells = next(cells)
-            }
-            val remaining = n % (day - 1)
-            for (i in 1 until remaining) {
-                cells = next(cells)
-            }
-            return cells
-        } else {
-            for (i in 1..n) {
-                cells = next(cells)
-                days[i] = cells
-            }
-            return days[n]!!
+            map[n + 1] = cells.joinToString(",")
+            n++
         }
+        if (_n > 14) {
+            n = _n % 14
+            if (n == 0) n = 14
+        } else {
+            n = _n
+        }
+        return map[n]!!.split(",").map { it.toInt() }.toIntArray()
     }
-}
 
-private fun next(_next: IntArray): IntArray {
-//    println(_next.contentToString())
-    val next = _next.copyOf()
-    for (idx in 1 until next.lastIndex) {
-        next[idx] = if (_next[idx - 1] == _next[idx + 1]) 1 else 0
+    fun actualPrisonAfterNDays(_cells: IntArray, n: Int): IntArray {
+        var cells = _cells
+        for (idx in 0 until n) {
+            cells = next(cells)
+        }
+        println(cells.joinToString(","))
+        return cells
     }
-    next[0] = 0
-    next[next.lastIndex] = 0
-    return next
+
+    private fun next(next: IntArray): IntArray {
+        val _next = next.copyOf()
+        for (idx in 1 until _next.lastIndex) {
+            _next[idx] = if (next[idx - 1] == next[idx + 1]) 1 else 0
+        }
+        _next[0] = 0
+        _next[_next.lastIndex] = 0
+        return _next
+    }
 }
 
 // Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
@@ -50,20 +49,29 @@ private fun next(_next: IntArray): IntArray {
 // Day 6: [0, 0, 1, 1, 0, 0, 0, 0]
 // Day 7: [0, 0, 0, 0, 0, 1, 1, 0]
 // Day 8: [0, 1, 1, 1, 0, 0, 0, 0]
-// Day 9: [0, 0, 1, 0, 0, 0, 0, 0]
-// Day 10: [0, 0, 1, 0, 0, 0, 0, 0]
-// Day 11: [0, 0, 1, 0, 1, 1, 1, 0]
-// Day 12: [0, 0, 1, 1, 0, 1, 0, 0]
+// Day 9: [0, 0, 1, 0, 1, 1, 0, 0]
+// Day 10: [0, 0, 1, 1, 0, 1, 0, 0]
+// Day 11: [0, 0, 0, 0, 1, 1, 1, 0]
+// Day 12: [0, 1, 1, 0, 0, 0, 0, 0]
 // Day 13: [0, 0, 0, 0, 1, 1, 0, 0]
-// Day 14: [0, 1, 1, 0, 0, 0, 0, 0]
-// Day 15: [0, 0, 0, 0, 1, 1, 1, 0]
-
+// Day 14: [0, 1, 1, 1, 0, 1, 0, 0]
 
 fun main() {
     val solution = Solution()
-    println(solution.prisonAfterNDays(intArrayOf(0, 1, 0, 1, 1, 0, 0, 1), 7).contentToString())
-    println(solution.prisonAfterNDays(intArrayOf(1, 0, 0, 1, 0, 0, 1, 0), 1000000000).contentToString())
+    if (solution.actualPrisonAfterNDays(
+            intArrayOf(1, 0, 0, 1, 0, 0, 1, 0), 9999999
+        ).contentEquals(
+            solution.prisonAfterNDays(
+                intArrayOf(1, 0, 0, 1, 0, 0, 1, 0), 9999999
+            )
+        )
+    ) {
+        println("true")
+    } else {
+        println("false")
+    }
+//    println(solution.prisonAfterNDays(intArrayOf(1, 0, 0, 1, 0, 0, 1, 0), 1000000000).contentToString())
 //    println(solution.prisonAfterNDays(intArrayOf(1, 0, 0, 1, 0, 0, 0, 1), 826).contentToString())
 //    println(solution.prisonAfterNDays(intArrayOf(1, 0, 0, 1, 0, 0, 1, 0), 1000000000).contentToString())
-    println(solution.prisonAfterNDays(intArrayOf(1, 1, 0, 1, 1, 0, 0, 1), 300663720).contentToString())
+//    println(solution.prisonAfterNDays(intArrayOf(1, 1, 0, 1, 1, 0, 0, 1), 300663720).contentToString())
 }
